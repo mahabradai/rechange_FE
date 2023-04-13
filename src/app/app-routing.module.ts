320fbc6e2f9d4dc8f2f050686503ee48a997ec14
mahabradai <br.maha.coder@gmail.com>
2023-04-13 16:06:27 +0200

    fixing menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent as MarqueHomeComponent } from './marque/home/home.component';
+import { HomeComponent as TypeHomeComponent } from './type/home/home.component';
+import { HomeComponent as PieceHomeComponent } from './piece/home/home.component';
+
+
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'marque/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'type',
-    redirectTo: 'type/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'piece',
-    redirectTo: 'piece/home',
-    pathMatch: 'full',
-  },
-];
+    { path:'marque-home' , component: MarqueHomeComponent },
+    { path:'type-home' , component: TypeHomeComponent },
+    { path:'piece-home' , component: PieceHomeComponent },
+  ];
+  
+
+
+  
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarqueModule } from './marque/marque.module';
 import { TypeModule } from './type/type.module';
 import { PieceModule } from './piece/piece.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ],
+    NavbarComponent,
+    
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MarqueModule,
-    TypeModule ,
+    TypeModule,
     PieceModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
